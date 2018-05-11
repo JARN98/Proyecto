71e72decb9e64360be53417453e652509563c875
juamarmor3 <juamarmor3@gmail.com>
2018-05-11 13:08:54 +0200

    RAR de ejemplo

--- goldtowerhotel/src/main/java/com/salesianostriana/goldtowerhotel/repository/HabitacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.salesianostriana.goldtowerhotel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesianostriana.goldtowerhotel.model.Habitacion;
-
-public interface HabitacionRepository extends JpaRepository<Habitacion, Long>{
-
-}
--- goldtowerhotel/src/main/java/com/salesianostriana/goldtowerhotel/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.salesianostriana.goldtowerhotel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesianostriana.goldtowerhotel.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-
-}
