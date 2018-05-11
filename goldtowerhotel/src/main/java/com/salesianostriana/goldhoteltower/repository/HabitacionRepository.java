ff2221494aa15a3b9b709206b14555fe9c25ca77
JARN98 <38279193+JARN98@users.noreply.github.com>
2018-05-11 16:28:04 +0200

    Limpieza

--- goldtowerhotel/src/main/java/com/salesianostriana/goldhoteltower/repository/HabitacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.salesianostriana.goldhoteltower.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesianostriana.goldtowerhotel.model.Habitacion;
-
-public interface HabitacionRepository extends JpaRepository<Habitacion, Long>{
-
-}
